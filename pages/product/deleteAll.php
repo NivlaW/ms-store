8e1104e09b0c64c876373cd48622fba3a9743e04
NivlaW <1nivla1alvin@gmail.com>
2024-05-09 00:52:55 +0700

    update multi delete and dashboard

--- pages/product/deleteAll.php
+++ pages/product/deleteAll.php
@@ -6,14 +6,14 @@ if(isset($_POST["id_produk"])){
         $quey = mysqli_query($connect, "SELECT * FROM tbl_produk WHERE id_produk = '$id'");
         $data = mysqli_fetch_array($quey);
 
-        unlink('assets/img/' . $data['foto']);
+        unlink('./src/images/foto-produk/' . $data['foto']);
 
         $delete = "DELETE FROM tbl_produk WHERE id_produk = ?";
         $proses = $connect->prepare($delete);
         $proses->bind_param("i",$id);
         $proses->execute();
     }
-    echo "<script language = 'JavaScript'>alert('Data Berhasil Dihapus');document.location='?page=data_produk'</script>";
+    echo "<script language = 'JavaScript'>alert('Data Berhasil Dihapus');document.location='?page=data_product'</script>";
 
 }
 ?>
